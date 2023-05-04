c59ccee194de944d519d24b8f103d8407c1bd395
Anthony Matheus <antyoma33@gmail.com>
2023-05-04 18:07:16 -0500

    Recibir información desde el cliente: Body y Query

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
+// const bodyParser = require('body-parser'); 
 const router = express.Router();
 
 var app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(router);
 
-router.get('/message', function (req, res) {
+app.get('/message', function (req, res) {
     res.send('Hola, bienvenid@ a la lista de mensajes del chat');
 });
 
-router.delete('/message', function (req, res) {
-    res.send('Mensajes añadidos del chat');
+app.delete('/message', function (req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    res.send('Mensaje ' + req.body.text + ' añadidos del chat');
 });
 
 app.listen(3000);
