1b999af3840a445bcd5c81e0e3764a81e79b10b3
Anthony Matheus <antyoma33@gmail.com>
2023-05-04 17:01:50 -0500

    Crear un servidor HTTP desde NodeJS

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require('express');
+
+var app = express();
+
+app.use('/', function (req, res) {
+    res.send('Hola, bienvenid@ a la creación de un chat con NodeJS');
+});
+
+app.listen(3000);
+console.log('La aplicación esta escuchando en http://localhost:3000');
