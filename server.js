b3404c2d8dab023a0750382f51454a5e335d0fad
Anthony Matheus <antyoma33@gmail.com>
2023-05-05 12:45:35 -0500

    Tipos de respuesta: Vacía, plana, con datos y estructurada

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.get('/message', function (req, res) {
     res.send('Hola, bienvenid@ a la lista de mensajes del chat');
 });
 
-app.delete('/message', function (req, res) {
+app.post('/message', function (req, res) {
     console.log(req.query);
     console.log(req.body);
-    res.send('Mensaje ' + req.body.text + ' añadidos del chat');
+    res.status(201).send([{error: '', body: 'Creado correctamente'}]);
 });
 
 app.listen(3000);
