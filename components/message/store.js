353fc03a23b609cd15bf14d2aa201f7ba91e0a0d
Anthony Matheus <antyoma33@gmail.com>
2023-05-06 22:24:25 -0500

    MongoDB: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,29 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+
+db.Promise = global.Promise;
+//--------------------------------------Conexión con la BD-------------------------------------------------
+db.connect('mongodb+srv://user:user1234@telegram.yvofkw3.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'telegram'
+});
+
+console.log('[db] conectada con exito');
+
+//---------------------------------------------------------------------------------------------------------
 
 function addMessage(message) {
-    list.push(message);
+    // list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    // return list;
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
