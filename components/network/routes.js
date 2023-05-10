09d3f572fd7382565ebe4cac2a2b142b44808fc4
Anthony Matheus <antyoma33@gmail.com>
2023-05-09 23:30:12 -0500

    Escalando la arquitectura: Múltiples entidades

--- components/network/routes.js
+++ components/network/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const message = require('../message/network');
+const user = require('../user/network');
 
 const routes = function (server) {
     server.use('/message', message);
+    server.use('/user', user);
 }
 
 module.exports = routes;
