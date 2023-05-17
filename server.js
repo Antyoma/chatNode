78b9ae7dc745404832cdbd4ba9bd6b138cfe96d7
Anthony Matheus <antyoma33@gmail.com>
2023-05-17 12:00:08 -0500

    Agregando cors

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+const cors = require('cors');
 // const bodyParser = require('body-parser');
 const socket = require('./socket');
 const db = require('./db')
 const router = require('./components/network/routes');
 
 db('mongodb+srv://user:user1234@telegram.yvofkw3.mongodb.net/?retryWrites=true&w=majority');
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
